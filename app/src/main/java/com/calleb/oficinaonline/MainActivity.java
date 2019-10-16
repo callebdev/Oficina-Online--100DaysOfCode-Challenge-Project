be93343eb4e6adf90702e39b59c22769ece615d5
callebdev <callebmiquissene@gmail.com>
2019-10-16 15:36:45 +0200

    Initial commit

---
+++ app/src/main/java/com/calleb/oficinaonline/MainActivity.java
@@ -0,0 +1,32 @@
+package com.calleb.oficinaonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_TIME);
+
+
+    }
+}
